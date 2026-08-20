eb0f13f59ad6018d5b93dae1b914e166844482e0
khamismgofi-web <khamismgofi18@gmail.com>
2026-08-21 02:36:10 +0300

    feat:import sqlachemy libraries

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -1,2 +1,7 @@
 # user SQLAlchemy model for MTEJA AI
+from sqlalchemy.orm import Mapped,mapped_column,Datetime,uuid,DeclarativeBase,relationship
+from sqlalchemy import String,datetime
+from datetime import datetime
+
+
 # Defines table schema, relationships, and multi-tenant organization_id
