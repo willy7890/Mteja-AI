1dc8a0ff09e22c28a34b1b629c58a90c44dc31b3
khamismgofi-web <khamismgofi18@gmail.com>
2026-08-31 12:59:07 +0300

    feat:adding navlinks

--- frontend/src/component/Navbar.jsx
+++ frontend/src/component/Navbar.jsx
@@ -2,6 +2,16 @@ import React,{useState} from 'react'
 import ThemeToggle from './ThemeToggle'
 
 function Navbar() {
+    const [isOpen, setIsOpen] = useState(false);
+
+    //Navlinks
+    const Navlinks = [
+        {name:'Theme', href: #ThemeToggle},
+        {name:'Home', href:#HomePage},
+        {name:'About',href:#AboutPage},
+        {name:'Login',href:#LoginPage},
+        {name:'Request Demo',href:#DemoPage}
+    ]
   return (
     <div>
       
