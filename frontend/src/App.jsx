0dc3fe9879ccda4be2bf4994886ff116cb7967b8
khamismgofi-web <khamismgofi18@gmail.com>
2026-08-31 13:51:33 +0300

    feat:create hero page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import Navbar from './component/Navbar'
+import Hero from './component/Hero'
 
 function App() {
   return (
     <div>
-      
+      <Navbar />
+      <Hero />
     </div>
   )
 }
