b54175defc7ac25fe8672ceebfabe430f74ab8f8
khamismgofi-web <khamismgofi18@gmail.com>
2026-09-04 11:56:01 +0300

    feat:adding about section

--- frontend/src/Pages/LandingPage.jsx
+++ frontend/src/Pages/LandingPage.jsx
@@ -1,15 +1,29 @@
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 import Hero from '../component/Hero';
 import TrustBar from '../component/TrustBar';
+import AboutSection from '../component/AboutSection';
 import FeatureGrid from '../component/FeatureGrid';
 import ProblemSection from '../component/ProblemSection';
-import Testimonial from '../component/Testimonial';
+import Testimonials from '../component/Testimonials';
 import FinalCta from '../component/FinalCta';
 
 function LandingPage({ t }) {
+  const location = useLocation();
+
+  
+  useEffect(() => {
+    if (!location.hash) return;
+    const id = location.hash.slice(1);
+    const el = document.getElementById(id);
+    if (el) el.scrollIntoView({ behavior: 'smooth' });
+  }, [location.hash]);
+
   return (
     <>
       <Hero t={t} />
       <TrustBar t={t} />
+      <AboutSection t={t} />
 
       <div style={{ background: t.sectionTint }}>
         <FeatureGrid t={t} />
@@ -18,7 +32,7 @@ function LandingPage({ t }) {
       <ProblemSection t={t} />
 
       <div style={{ background: t.sectionTint }}>
-        <Testimonial t={t} />
+        <Testimonials t={t} />
       </div>
 
       <FinalCta t={t} />
